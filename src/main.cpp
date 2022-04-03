741b7ffab552b5ab6beffb9d215955daf419061f
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2022-04-03 00:57:06 -0400

    Adding after saving

--- src/main.cpp
+++ src/main.cpp
@@ -3,97 +3,112 @@
 int threeMinButton = 3;
 int threeAndTwo = 6;
 int relayOutput = 7;
+int millSeconds;
 
+// Function Definitions
 void runThreeMinutes();
-void runThreeAndTwo();
+// void runThreeAndTwo();
 
-void ThreeMinHorn();
-void TwoMinHorn();
-void OneMinHorn();
+void horn(int);
+
 
 void setup() {
   Serial.begin(9600);
-
   pinMode(threeMinButton, INPUT); // Input button for runThreeMinutes()
   pinMode(threeAndTwo, INPUT); // Input botton for runThreeAndTwo()
-
   pinMode(relayOutput, OUTPUT); // Output for Relay for horn
-
 }
 
 void loop() {
-  if(digitalRead(threeMinButton) == HIGH){
-    runThreeMinutes();
-    Serial.write("Three Min putton Pressed");
-  }
-  else if(digitalRead(threeAndTwo) == HIGH){
-    runThreeAndTwo();
-    Serial.write("Two min putton Pressed");
-  }
-}
-
-/////////////////////////////
-//   Starting Sequences    //
-/////////////////////////////
-void runThreeMinutes(){ // Fucntion to run a 3 minute starting sequence 
-  ThreeMinHorn();
-  TwoMinHorn();
-  OneMinHorn();
 
 }
 
-void runThreeAndTwo(){ // Function to run 3 concective 2 minute starts 
-  // Sequence #1
-  TwoMinHorn();
-  OneMinHorn();
-
-  // Sequence #2
-  TwoMinHorn();
-  OneMinHorn();
+void horn(int millSeconds){
+  digitalWrite(7,HIGH);
+  delay(millSeconds);
+  digitalWrite(7, LOW);
 }
 
 
-/////////////////////////
-//   Horn Functions    //
-/////////////////////////
-void ThreeMinHorn(){
-// First Horn
-  digitalWrite(7,HIGH);
-  delay(1000);
-  digitalWrite(12, LOW);
+void runThreeMinutes(){
+  // 3 min Horn
+  horn(1000);
   delay(500);
-  // Second  Horn
-  digitalWrite(7,HIGH);
-  delay(1000);
-  digitalWrite(12, LOW);
+  horn(1000);
   delay(500);
-  // Thrid Horn
-  digitalWrite(7,HIGH);
-  delay(1000);
-  digitalWrite(12, LOW);
+  horn(1000);
+
+  // 1 min space
+  delay(60000);
+
+  // 2 min Horn
+  horn(1000);
   delay(500);
+  horn(1000);
+
+  // :30 sec space
+  delay(30000);
+
+  // 1:30 Horn
+  horn(1000);
+  delay(500);
+
+  horn(500);
+  delay(200);
+  horn(500);
+  delay(200);
+  horn(500);
+  
+  // :30 sec space
+  delay(30000);
+
+  // 1:00 Horn
+  horn(1000);
+
+  // :30 sec space
+  delay(30000);
+
+  // :30 Horn
+  horn(500);
+  delay(200);
+  horn(500);
+  delay(200);
+  horn(500);
+
+  // :10 sec space
+  delay(10000);
+
+  // :20 Horn
+  horn(500);
+  delay(200);
+  horn(500);
+
+  // :10 sec space
+  delay(10000);
+
+  // :10 Horn
+  horn(500);
+
+  // :5 sec space
+  delay(5000);
+
+  // :5,4,3,2,1
+  horn(250);
+  delay(100);
+  horn(250);
+  delay(100);
+  horn(250);
+  delay(100);
+  horn(250);
+  delay(100);
+  horn(250);
+ 
+  delay(100);
+
+  // Go!
+  horn(10000);
+
+
+  
 }
-
-void TwoMinHorn(){
-// First Horn
-  digitalWrite(7,HIGH);
-  delay(1000);
-  digitalWrite(12, LOW);
-  delay(500);
-  // Second  Horn
-  digitalWrite(7,HIGH);
-  delay(1000);
-  digitalWrite(12, LOW);
-  delay(500);
-}
-
-void OneMinHorn(){
-// One Min Horn
-  digitalWrite(7,HIGH);
-  delay(1000);
-  digitalWrite(12, LOW);
-  delay(500);
-}
-
-
 
